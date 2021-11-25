3d0090be291f9757b5ff252c11d9edf9b8b9a51b
vsinky <sinkykumariverma@gmail.com>
2021-11-25 20:43:22 +0530

    Added: peak and pop

--- src/com/bridgelabz/stacks/Main.java
+++ src/com/bridgelabz/stacks/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stack and Queue Problems");
-		Stack stack = new Stack(3);
+		Stack stack = new Stack(5);
 		stack.push(56);
 		stack.push(30);
 		stack.push(70);
-
+		System.out.println(stack.pop());
 		stack.push(80);
+		System.out.println(stack.peek());
 
 		System.out.println(stack.isFull());
-
+		System.out.println(stack.size());
 	}
-
 }
--- src/com/bridgelabz/stacks/Stack.java
+++ src/com/bridgelabz/stacks/Stack.java
@@ -3,12 +3,12 @@ package com.bridgelabz.stacks;
 public class Stack {
 	private static final int MAX = 10;
 
-	private int[] array;  
+	private int[] array; 
 	private int top;
 
 	public Stack() {
 		array = new int[MAX];
-		top = -1;   
+		top = -1; 
 	}
 
 	public Stack(int length) {
@@ -18,18 +18,53 @@ public class Stack {
 
 	public boolean push(int data) {
 
-		if (isFull()) { 
+		if (isFull()) {
 			System.out.println("Stack is overflow");
 			return false;
 		}
-		array[++top] = data;  
-		System.out.println("pushed data into stack " +data);
+		array[++top] = data;
+		System.out.println("pushed data into stack " + data);
 		return true;
 
 	}
 
 	public boolean isFull() {
 
-		return top >= array.length - 1; 
+		return top >= array.length - 1;
+	}
+
+	public int pop() {
+
+		if (isEmpty()) {
+			System.out.println("Stack is underflow");
+			return 0;
+		}
+
+		int data = array[top++];
+		return data;
+	}
+
+	public int peek() {
+		if (isEmpty()) {
+			System.out.println("Stack is underflow");
+			return 0;
+		}
+		int data = array[top++];
+		return data;
+	}
+
+	private boolean isEmpty() {
+
+		return top < 0;
+	}
+
+	public int size() {
+		if (isEmpty()) {
+
+			return 0;
+		}
+
+		return ++top;
+
 	}
 }
